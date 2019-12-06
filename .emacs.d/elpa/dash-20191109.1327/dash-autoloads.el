673387be76919def4ba789f1565cba237a44e032
letranger <yen.yungchin@gmail.com>
2019-12-06 19:07:26 +0800

    Origin version

---
+++ .emacs.d/elpa/dash-20191109.1327/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dash.el") (24041 604 780665 387000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
---
+++ .emacs.d/elpa/eval-in-repl-20191116.1107/eval-in-repl-autoloads.el
@@ -0,0 +1,236 @@
+;;; eval-in-repl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "eval-in-repl-cider" "eval-in-repl-cider.el"
+;;;;;;  (24041 606 696694 55000))
+;;; Generated autoloads from eval-in-repl-cider.el
+
+(autoload 'eir-eval-in-cider "eval-in-repl-cider" "\
+eval-in-repl for cider.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-elm" "eval-in-repl-elm.el" (24041
+;;;;;;  606 568692 140000))
+;;; Generated autoloads from eval-in-repl-elm.el
+
+(autoload 'eir-eval-in-elm "eval-in-repl-elm" "\
+Provides eval-in-repl for Elm.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-erlang" "eval-in-repl-erlang.el"
+;;;;;;  (24041 606 460690 524000))
+;;; Generated autoloads from eval-in-repl-erlang.el
+
+(autoload 'eir-eval-in-erlang "eval-in-repl-erlang" "\
+Provides eval-in-repl for Erlang.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-geiser" "eval-in-repl-geiser.el"
+;;;;;;  (24041 606 508691 242000))
+;;; Generated autoloads from eval-in-repl-geiser.el
+
+(autoload 'eir-eval-in-geiser "eval-in-repl-geiser" "\
+eval-in-repl for Geiser.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-hy" "eval-in-repl-hy.el" (24041
+;;;;;;  606 688693 936000))
+;;; Generated autoloads from eval-in-repl-hy.el
+
+(autoload 'eir-eval-in-hy "eval-in-repl-hy" "\
+eval-in-repl for Hy.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-ielm" "eval-in-repl-ielm.el"
+;;;;;;  (24041 606 556691 960000))
+;;; Generated autoloads from eval-in-repl-ielm.el
+
+(autoload 'eir-eval-in-ielm "eval-in-repl-ielm" "\
+eval-in-repl for IELM.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-iex" "eval-in-repl-iex.el" (24041
+;;;;;;  606 496691 63000))
+;;; Generated autoloads from eval-in-repl-iex.el
+
+(autoload 'eir-eval-in-iex "eval-in-repl-iex" "\
+Provides eval-in-repl for Elixir.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-javascript" "eval-in-repl-javascript.el"
+;;;;;;  (24041 606 592692 499000))
+;;; Generated autoloads from eval-in-repl-javascript.el
+
+(autoload 'eir-eval-in-javascript "eval-in-repl-javascript" "\
+eval-in-repl for Javascript.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-lua" "eval-in-repl-lua.el" (24041
+;;;;;;  606 580692 320000))
+;;; Generated autoloads from eval-in-repl-lua.el
+
+(autoload 'eir-eval-in-lua "eval-in-repl-lua" "\
+eval-in-repl for Lua.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-ocaml" "eval-in-repl-ocaml.el"
+;;;;;;  (24041 606 652693 397000))
+;;; Generated autoloads from eval-in-repl-ocaml.el
+
+(autoload 'eir-eval-in-ocaml "eval-in-repl-ocaml" "\
+eval-in-repl for OCaml.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-prolog" "eval-in-repl-prolog.el"
+;;;;;;  (24041 606 640693 217000))
+;;; Generated autoloads from eval-in-repl-prolog.el
+
+(autoload 'eir-eval-in-prolog "eval-in-repl-prolog" "\
+eval-in-repl for SWI Prolog.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-python" "eval-in-repl-python.el"
+;;;;;;  (24041 606 676693 756000))
+;;; Generated autoloads from eval-in-repl-python.el
+
+(autoload 'eir-eval-in-python "eval-in-repl-python" "\
+eval-in-repl for Python.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-racket" "eval-in-repl-racket.el"
+;;;;;;  (24041 606 520691 421000))
+;;; Generated autoloads from eval-in-repl-racket.el
+
+(autoload 'eir-eval-in-racket "eval-in-repl-racket" "\
+eval-in-repl for Racket.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-ruby" "eval-in-repl-ruby.el"
+;;;;;;  (24041 606 616692 858000))
+;;; Generated autoloads from eval-in-repl-ruby.el
+
+(autoload 'eir-eval-in-ruby "eval-in-repl-ruby" "\
+eval-in-repl for Ruby.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-scala" "eval-in-repl-scala.el"
+;;;;;;  (24041 606 604692 679000))
+;;; Generated autoloads from eval-in-repl-scala.el
+
+(autoload 'eir-eval-in-scala "eval-in-repl-scala" "\
+Provides eval-in-repl for Scala.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-scheme" "eval-in-repl-scheme.el"
+;;;;;;  (24041 606 664693 577000))
+;;; Generated autoloads from eval-in-repl-scheme.el
+
+(autoload 'eir-eval-in-scheme "eval-in-repl-scheme" "\
+eval-in-repl for Scheme.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-shell" "eval-in-repl-shell.el"
+;;;;;;  (24041 606 628693 38000))
+;;; Generated autoloads from eval-in-repl-shell.el
+
+(autoload 'eir-eval-in-shell "eval-in-repl-shell" "\
+eval-in-repl for shell.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-slime" "eval-in-repl-slime.el"
+;;;;;;  (24041 606 528691 542000))
+;;; Generated autoloads from eval-in-repl-slime.el
+
+(autoload 'eir-eval-in-slime "eval-in-repl-slime" "\
+eval-in-repl for SLIME.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-sly" "eval-in-repl-sly.el" (24041
+;;;;;;  606 544691 781000))
+;;; Generated autoloads from eval-in-repl-sly.el
+
+(autoload 'eir-eval-in-sly "eval-in-repl-sly" "\
+eval-in-repl for Sly.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "eval-in-repl-sml" "eval-in-repl-sml.el" (24041
+;;;;;;  606 472690 704000))
+;;; Generated autoloads from eval-in-repl-sml.el
+
+(autoload 'eir-eval-in-sml "eval-in-repl-sml" "\
+eval-in-repl for Standard ML.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("eval-in-repl-pkg.el" "eval-in-repl.el")
+;;;;;;  (24041 606 484690 883000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; eval-in-repl-autoloads.el ends here
